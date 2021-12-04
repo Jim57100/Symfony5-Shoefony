e6d4dcb0a2416aeef072d2cad77a078ab318d3b6
Jean-Michael SALICE <jmsalice@gmail.com>
2021-12-04 11:27:54 +0100

    store request finished

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1); //bonne pratique de typer fort
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -7,25 +9,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class StoreController extends AbstractController
+final class StoreController extends AbstractController
 {
 
     #[Route('/store/product/{id}/details/{slug}', name: 'show_product', methods: ['GET'], requirements: ['id' => '\d+'])]
     public function show_product(Request $request, int $id, string $slug): Response
     {
-        $url = $this->generateUrl('show_product', [
-            'id' => $id,
-            'slug' => $slug
-            
-        ]);
-
-        $ip= $request->getClientIp();
-
         return $this->render('store/show_product.html.twig', [ 
             'id' => $id,
             'slug' => $slug,
-            'uri' => $url,
-            'ip' => $ip
+            'url' => $request->getUri(),
+            'ip' => $request->getClientIp(),
+            'from routing' => $this->generateUrl('show_product', [
+                'id' => $id,
+                'slug' => $slug  
+            ])
+
         ]);
     }
 
